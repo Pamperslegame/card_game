9bffa4cab2bf235da201619b70bb2205d9513fed
GregAuRizzz <gregory_duporge@ens.univ-artois.fr>
2025-01-18 01:12:45 +0100

    menu ui

---
+++ Assets/ButtonAction.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAction : MonoBehaviour
+{
+    public void OnButton1Click()
+    {
+        Debug.Log("bouton start cliqué");
+    }
+
+}
