d6610f0a48b4599f0815941b1d8bd510a47cbbc2
Julien Biout <35117117+Pamperslegame@users.noreply.github.com>
2025-02-10 02:03:21 +0100

    Maj du Layout

--- Assets/Scripts/CardDefinition.cs
+++ Assets/Scripts/CardDefinition.cs
@@ -8,19 +8,22 @@ public class CardDefinition : ScriptableObject
     [SerializeField] private int damage;
     [SerializeField] private int cost;
     [SerializeField] private Sprite cardImage;
+    [SerializeField] private Sprite cadreImage;
 
     public int Health => health;
     public int MaxHealth => maxHealth;
     public int Damage => damage;
     public int Cost => cost;
     public Sprite CardImage => cardImage;
+    public Sprite CadreImage => cadreImage;
 
-    public void Initialize(int health, int maxHealth, int damage, int cost, Sprite image)
+    public void Initialize(int health, int maxHealth, int damage, int cost, Sprite image, Sprite cadre)
     {
         this.health = health;
         this.maxHealth = maxHealth;
         this.damage = damage;
         this.cost = cost;
         this.cardImage = image;
+        this.cadreImage = cadre;
     }
 }
--- Assets/Scripts/CardInitialisation.cs
+++ Assets/Scripts/CardInitialisation.cs
@@ -5,14 +5,43 @@ using UnityEngine;
 public class Card : MonoBehaviour
 {
     public CardDefinition CardDefinition;
+    public GameObject Cadre;
+    public GameObject Image;
+
+    private SpriteRenderer cadreRenderer;
+    private SpriteRenderer imageRenderer;
 
     void Start()
     {
-
+        if (Cadre != null) cadreRenderer = Cadre.GetComponent<SpriteRenderer>();
+        if (Image != null) imageRenderer = Image.GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
+        if (CardDefinition != null)
+        {
+            if (cadreRenderer != null)
+            {
+                cadreRenderer.sprite = CardDefinition.CadreImage;
+            }
+            else
+            {
+                Debug.LogWarning("SpriteRenderer du cadre non trouvé !");
+            }
 
+            if (imageRenderer != null)
+            {
+                imageRenderer.sprite = CardDefinition.CardImage;
+            }
+            else
+            {
+                Debug.LogWarning("SpriteRenderer de l'image non trouvé !");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Aucune définition de carte assignée !");
+        }
     }
 }
