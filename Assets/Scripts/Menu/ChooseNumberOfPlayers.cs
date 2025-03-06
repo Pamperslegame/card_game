b6dbd73d0619594f08104f871dc093cb5b1fc890
GregAuRizzz <gregory_duporge@ens.univ-artois.fr>
2025-03-06 10:49:19 +0100

    Reorganization of canvas and management of the number of players

--- Assets/Scripts/Menu/ChooseNumberOfPlayers.cs
+++ Assets/Scripts/Menu/ChooseNumberOfPlayers.cs
@@ -5,11 +5,9 @@ public class ChooseNumberOfPlayers : MonoBehaviour
 {
     public void SetPlayerCount(int count)
     {
-        // Sauvegarder le nombre de joueurs dans PlayerPrefs
         PlayerPrefs.SetInt("PlayerCount", count);
         PlayerPrefs.Save();
 
-        // Charger la scène suivante (choisir pseudo et avatar)
         SceneManager.LoadScene("ChoosePlayers");
     }
 }
